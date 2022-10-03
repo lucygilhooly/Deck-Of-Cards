5b8d037f57183ce02ee06ed64aa796e759735f9f
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-03 12:24:20 +0100

    make deck of cards successfully

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -3,24 +3,38 @@ package org.example;
 import java.util.Arrays;
 
 public class Card {
+// need a card to be generated with a suit, a value and a symbol
+    //possibly using enums for the suit
 
-    private final String[] suit;// = {"spades", "hearts", "clubs", "diamonds"};
+    private final CardGame.suits suit;
 
-    private final String[] symbol;// ={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};;
-    private final int[] value;// = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+    private final String symbol;// ={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};;
+    private final int value;// = {2,3,4,5,6,7,8,9,10,11,12,13,14};
 
-    public Card(String[] suit, String[] symbol, int[] value) {
+    public Card(CardGame.suits suit, String symbol, int value) {
         this.suit = suit;
         this.symbol = symbol;
         this.value = value;
     }
 
+    public CardGame.suits getSuit() {
+        return suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
     @Override
     public String toString() {
         return "Card{" +
-                "suit=" + Arrays.toString(suit) +
-                ", symbol=" + Arrays.toString(symbol) +
-                ", value=" + Arrays.toString(value) +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
                 '}';
     }
 }
--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -2,18 +2,38 @@ package org.example;
 import java.util.ArrayList;
 
 public class CardGame {
-
-    private enum suits {
+// uses the card class to generate a full pack
+    enum suits {
             SPADES, HEARTS, CLUBS, DIAMONDS
     }
+    private String name;
+    private final String[] symbols = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private final int[] values = new int[] {2,3,4,5,6,7,8,9,10,11,12,13,14};
     static ArrayList<Card> cardDeck = new ArrayList<>();
+    public CardGame(String name) {
+        this.name= name;
+        populateCardDeck();
+    }
+
+    void populateCardDeck(){
+        for(suits suit : suits.values()){
+            for (int i = 0; i < symbols.length; i++) {
+                Card card = new Card(suit,symbols[i],values[i]);
+                cardDeck.add(card);
+            }
+        }
+
+        for (int i = 0; i < cardDeck.size(); i++) {
+            System.out.println(cardDeck.get(i));
+        }
+    }
 
     public ArrayList<Card> getCardDeck() {
         return cardDeck;
     }
 
     public static void main(String[] args) {
-
+        CardGame cards = new CardGame("SNAP");
     }
 
 }
