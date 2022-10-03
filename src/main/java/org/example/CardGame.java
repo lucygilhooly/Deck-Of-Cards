df42f3cd23f09bbc68d16164b1b75def9581c433
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-03 13:00:12 +0100

    Deal card method functions

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -1,8 +1,8 @@
 package org.example;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CardGame {
-// uses the card class to generate a full pack
     enum suits {
             SPADES, HEARTS, CLUBS, DIAMONDS
     }
@@ -10,6 +10,8 @@ public class CardGame {
     private final String[] symbols = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
     private final int[] values = new int[] {2,3,4,5,6,7,8,9,10,11,12,13,14};
     static ArrayList<Card> cardDeck = new ArrayList<>();
+    ArrayList<Card> deltCards = new ArrayList<>();
+
     public CardGame(String name) {
         this.name= name;
         populateCardDeck();
@@ -31,9 +33,27 @@ public class CardGame {
     public ArrayList<Card> getCardDeck() {
         return cardDeck;
     }
+    void dealCard(){
+        deltCards.add(cardDeck.get(0));
+        cardDeck.remove(0);
+        System.out.println(deltCards);
+        System.out.println(cardDeck);
+    }
+
+//    private static final Random RANDOM = new Random();
+//
+//    Card shuffleDeck(){
+//        return cardDeck.get(RANDOM.nextInt(cardDeck.size()));
+//
+//    }
+
+
 
     public static void main(String[] args) {
         CardGame cards = new CardGame("SNAP");
+        cards.dealCard();
+        cards.dealCard();
+        System.out.println(cards.deltCards);
     }
 
 }
